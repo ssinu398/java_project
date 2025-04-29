10a2ec3fa014ccb10b4ce03e3406d03de6d26fd1
ssinu398 <ssinu398@gmail.com>
2025-04-29 22:58:57 +0530

    Update CalculatorTest.java
    unit test

--- src/test/java/in/javahome/myweb/controller/CalculatorTest.java
+++ src/test/java/in/javahome/myweb/controller/CalculatorTest.java
@@ -1,7 +1,7 @@
 package in.javahome.myweb.controller;
 
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class CalculatorTest extends TestCase {
 	Calculator cal = new Calculator();
